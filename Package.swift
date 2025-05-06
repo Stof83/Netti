8020cc84609a803f1ea91fe9c1245e7c254eafb5
El Mostafa El Ouatri <mostafa@wetaxi.it>
2025-05-06 22:52:17 +0200

    - Updated platforms to require at least iOS 14.0, tvOS 14.0, watchOS 7.0, and macCatalyst 14.0 - Ensured compatibility with `os.Logger`, which is only available from iOS 14.0 and newer - Updated macOS deployment target to 11.0 to align with other platform requirements

--- Package.swift
+++ Package.swift
@@ -7,10 +7,10 @@ let package = Package(
     name: "Netti",
     platforms: [
         .macOS(.v11),
-        .iOS(.v13),
-        .tvOS(.v13),
-        .watchOS(.v6),
-        .macCatalyst(.v13),
+        .iOS(.v14),
+        .tvOS(.v14),
+        .watchOS(.v7),
+        .macCatalyst(.v14),
     ],
     products: [
         .library(name: "Netti", targets: ["Netti"])
